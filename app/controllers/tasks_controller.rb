fa59675d30091de800264ea349f179352e271c8d
fenjiot <fenjiot@gmail.com>
2014-10-21 11:54:08 -0400

    User can create tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,23 @@
 class TasksController < ApplicationController
   def index
     @tasks = current_user.tasks
+    @task = Task.new
+  end
+
+  def create
+    task = current_user.tasks.new(task_params)
+    if task.save
+      redirect_to :back
+    else
+    @tasks = current_user.tasks
+    @task = Task.new
+      render :index
+    end
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :description)
   end
 end
