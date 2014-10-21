5408a8ea0a320be128b650b3b4140fc3d71d7f9c
fenjiot <fenjiot@gmail.com>
2014-10-21 14:26:27 -0400

    Add error messages and validation + show incomplete

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,16 +1,21 @@
 class TasksController < ApplicationController
   def index
     @tasks = current_user.tasks
+    @incomplete_tasks = current_user.tasks.incomplete
+    @completed_tasks = current_user.tasks.completed
     @task = Task.new
   end
 
   def create
     task = current_user.tasks.new(task_params)
+
     if task.save
       redirect_to :back
     else
-    @tasks = current_user.tasks
-    @task = Task.new
+      @tasks = current_user.tasks.reload
+      @task = task
+      @incomplete_tasks = current_user.tasks.incomplete
+      @completed_tasks = current_user.tasks.completed
       render :index
     end
   end
